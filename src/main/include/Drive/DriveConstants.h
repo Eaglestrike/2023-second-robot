c9ef19853d2a2217a43c23a822d674af65f6864a
linc-mvla <100027020@mvla.net>
2023-07-15 13:46:00 -0700

    whoops forgor to save

--- src/main/include/Drive/DriveConstants.h
+++ src/main/include/Drive/DriveConstants.h
@@ -1,10 +1,10 @@
-#pragma once
-
-#include <cstddef>
-
 /**
  * A file of constants for the drivebase
  */
+
+#pragma once
+
+#include <cstddef>
 
 namespace SwerveConstants
 {
--- src/main/include/Util/UtilConstants.h
+++ src/main/include/Util/UtilConstants.h
@@ -1,8 +1,9 @@
-#pragma once
 
 /**
- * A file of constants for helper functions
+ * A file of general constants for helper functions
  */
+
+#pragma once
 
 namespace UtilConstants{
     const double NEAR_ZERO_TOLERANCE = 0.000001;

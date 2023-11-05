280673d78d5b7c04985734a0535c8b025debf8a0
Jonathan Liu <jonathanhliu21@gmail.com>
2023-11-04 19:39:11 -0700

    add lidar

--- src/main/cpp/Auto/BaseAuto.cpp
+++ src/main/cpp/Auto/BaseAuto.cpp
@@ -3,9 +3,10 @@
 BaseAuto::BaseAuto(ElevatorIntake &ei, AutoLineup &al, AutoPath &ap, Rollers &r)
     : m_curAng{0}, m_curTilt{0}, m_ei{ei}, m_al{al}, m_ap{ap}, m_r{r} {}
 
-void BaseAuto::UpdateOdom(vec::Vector2D pos, double ang, vec::Vector2D wheelVel, double tilt) {
+void BaseAuto::UpdateOdom(vec::Vector2D pos, double ang, vec::Vector2D wheelVel, double tilt, LidarReader::LidarData lidarData) {
     m_curPos = pos;
     m_curAng = ang;
     m_curWheelVel = wheelVel;
     m_curTilt = tilt;
+    m_lidarData = lidarData;
 }
--- src/main/include/Auto/BaseAuto.h
+++ src/main/include/Auto/BaseAuto.h
@@ -4,6 +4,7 @@
 #include "Drive/AutoPath.h"
 #include "Elevator/ElevatorIntake.h"
 #include "Elevator/Intake/Rollers.h"
+#include "Elevator/Lidar/LidarReader.h"
 
 #include "Util/thirdparty/simplevectors.hpp"
 
@@ -15,7 +16,7 @@ public:
 
     virtual void Init();
     virtual void Periodic();
-    void UpdateOdom(vec::Vector2D pos, double ang, vec::Vector2D wheelVel, double tilt);
+    void UpdateOdom(vec::Vector2D pos, double ang, vec::Vector2D wheelVel, double tilt, LidarReader::LidarData lidarData);
 
     virtual vec::Vector2D GetDriveVel();
     virtual double GetAngVel();
@@ -25,6 +26,7 @@ protected:
     double m_curAng; 
     vec::Vector2D m_curWheelVel;
     double m_curTilt;
+    LidarReader::LidarData m_lidarData;
 
     ElevatorIntake &m_ei;
     AutoLineup &m_al;
